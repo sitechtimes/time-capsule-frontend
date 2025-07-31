02735e7a81a10aeffe898f8282b12488cafd45ad
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2025-07-31 10:40:33 -0400

    es lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@nuxt/eslint"],
   vite: {
     plugins: [tailwindcss()],
   },
