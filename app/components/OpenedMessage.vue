51702d1db47714378761e2953f73e040e9714c68
christina chen <158211764+christinac09@users.noreply.github.com>
2025-09-29 10:57:23 -0400

    find way to identify clicked message to display in OpenedMessage

--- app/components/OpenedMessage.vue
+++ app/components/OpenedMessage.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <p>{{ message }}</p>
-    <!-- photos - have approve& report for each, or maybe they are automatically approved unless reported -->
+    <!-- photos here - have approve& report for each, or maybe they are automatically approved unless reported -->
     <button class="btn">approve all</button>
     <button class="btn">report all</button>
     <button class="btn" @click="$emit('close')">close</button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{
+  message: string;
+}>();
+</script>
 
 <style scoped></style>
--- app/pages/admin-inbox.vue
+++ app/pages/admin-inbox.vue
@@ -1,17 +1,64 @@
 <template>
-  <div class="flex flex-row">
-    <!-- messages -->
-    <div class="">
-      <InboxMessage name="test" :photo-count="0" time="test" @clicked="messageOpened = true" />
+  <div class="flex h-screen overflow-hidden">
+    <div class="w-80 overflow-y-auto border-r border-gray-300 bg-gray-100">
+      <InboxMessage v-for="message in messages" :name="message.student" :photo-count="message.photos.length" time="test" @clicked="messageOpened = true" />
     </div>
-    <!-- opened message, only show if a messsage is clicked on -->
-    <div v-if="!messageOpened">click on a message to open it</div>
-    <OpenedMessage v-else @close="messageOpened = false" />
+    <div class="flex-1 overflow-y-auto p-6">
+      <div v-if="!messageOpened" class="flex h-full items-center justify-center text-lg text-gray-500">Click on a message to open it</div>
+      <OpenedMessage message="test" v-else @close="messageOpened = false" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+// only allow open if role is admin
 const messageOpened = ref(false);
-</script>
+definePageMeta({
+  layout: "dashboard"
+});
 
-<style scoped></style>
+// fetch messages
+interface Message {
+  student: string;
+  time: string;
+  photos: Photo[];
+}
+
+//temporary lines 27-55 -will update later
+function formatPhotoDate(photos: PhotoResponse[]) {
+  return photos.map((item) => ({
+    ...item,
+    uploadDate: new Date(item.uploadDate * 1000)
+  }));
+}
+const photoData = ref<Photo[]>([]);
+const user = useUserStore().user;
+async function fetchPhotoData() {
+  const { data, error } = await tryRequestEndpoint<PhotoResponse[]>("/photos");
+  if (error) return error;
+  let newPhotoArray = formatPhotoDate(data);
+  if (user?.userType === "user") {
+    newPhotoArray = newPhotoArray.filter((photo) => photo.author === user.id);
+  } // this shouldn't be in frontend - filter by user with endpoint
+  photoData.value.push(...newPhotoArray);
+}
+interface PhotoResponse {
+  id: number;
+  uploadDate: number;
+  graduationYear: number;
+  event: string;
+  location: string;
+  people: string[];
+  imageData: string;
+  author: number;
+}
+onMounted(fetchPhotoData);
+
+const messages = ref<Message[]>([
+  {
+    student: "test",
+    time: "test",
+    photos: photoData.value
+  }
+]);
+</script>
