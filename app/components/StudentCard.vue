0f397cff8034715702d1f8054a9a0f096dff0f78
christina <158211764+christinac09@users.noreply.github.com>
2025-10-24 16:18:40 -0400

    got last uploaded; figure out getMostRecentUploadDate error handling

--- app/components/StudentCard.vue
+++ app/components/StudentCard.vue
@@ -2,7 +2,7 @@
   <div class="card w-[20%]">
     <h3>{{ student.firstName }} {{ student.lastName }}</h3>
     <p>{{ student.graduationYear }} ({{ currentGrade }})</p>
-    <p>last uploaded: {{ getMostRecentUploadDate(studentUploads) }}</p>
+    <p>last uploaded: {{ mostRecentUploadDate }}</p>
     <p>total uploads: {{ studentUploads?.length }}</p>
     <button @click="openStudentPage(student)" class="btn">See More</button>
   </div>
@@ -17,26 +17,44 @@ const store = useStudentStore();
 const currentGrade = ref<number | string>();
 currentGrade.value = getStudentGrade(props.student.graduationYear);
 const studentUploads = ref<Photo[]>();
+const mostRecentUploadDate = ref<string>("");
 
+interface PhotoResponse {
+  id: number;
+  uploadDate: number;
+  graduationYear: number;
+  event: string;
+  location: string;
+  people: string[];
+  imageData: string;
+  author: string;
+}
+function formatPhotoDate(photos: PhotoResponse[]) {
+  return photos.map((item) => ({
+    ...item,
+    uploadDate: new Date(item.uploadDate * 1000)
+  }));
+}
 async function fetchIndividualStudentUploads(name: string) {
-  // /photos endpoint should filter for specific student by name (or id?)
-  // const { data, error } = await tryRequestEndpoint<Photo[]>(`/photos/${author}`);
-  // lines 25-26 below are temporary
-  const { data, error } = await tryRequestEndpoint<Photo[]>("/photos");
-  const filteredData = data?.filter((photo) => photo.author === name);
+  const { data, error } = await tryRequestEndpoint<PhotoResponse[]>("/photos");
   if (error) return error;
-  studentUploads.value = filteredData;
-  store.studentUploads = filteredData;
+  const filteredData = data?.filter((photo) => photo.author === name);
+  const newPhotoArray = formatPhotoDate(filteredData);
+  console.log(newPhotoArray);
+  studentUploads.value = newPhotoArray;
+  store.studentUploads = newPhotoArray;
+}
+
+function getMostRecentUploadDate(photos: Photo[]): string {
+  // does this assume photos is not empty, not undefined, or both???
+  const mostRecentPhoto = photos.reduce((max, photo) => (photo.uploadDate > max.uploadDate ? photo : max));
+  return mostRecentPhoto.uploadDate.toLocaleDateString();
 }
 onMounted(async () => {
   const name = `${props.student.firstName} ${props.student.lastName}`;
   await fetchIndividualStudentUploads(name);
+  mostRecentUploadDate.value = studentUploads.value?.length ? getMostRecentUploadDate(studentUploads.value) : "N/A - student has no uploads";
 });
-
-function getMostRecentUploadDate(photos: Photo[]) {
-  const mostRecentPhotoDate = photos.reduce((max, photo) => (photo.uploadDate > max ? photo.uploadDate : max.uploadDate));
-  -
-}
 
 const router = useRouter();
 function openStudentPage(student: Student) {
