f9df12006b9630e03963866b117418d8c5b678eb
christina chen <158211764+christinac09@users.noreply.github.com>
2025-11-05 10:33:23 -0500

    tina changes

--- app/components/OpenedMessage.vue
+++ app/components/OpenedMessage.vue
@@ -3,11 +3,11 @@
     <p>{{ name }} uploaded {{ photos.length }} photos at [time] on [date]</p>
     <!-- have approve& report for each, or automatically approved unless reported? -->
     <p>{{ photos.length }} uploads:</p>
-    <div class="flex flex-row"><MessagePhoto v-for="photo in photos" :photo-data="photo" @clicked="openModal(photo)" @approve="approveOne(photo)" @report="reportOne(photo)" /></div>
+    <div class="flex flex-row"><MessagePhoto v-for="photo in photos" :photo-data="photo" @clicked="openModal(photo)" @approve="approve([photo])" @report="report([photo])" /></div>
 
     <div>
-      <button class="btn" @click="approveAll(photos)">Approve All</button>
-      <button class="btn" @click="reportAll(photos)">Report All</button>
+      <button class="btn" @click="approve(photos)">Approve All</button>
+      <button class="btn" @click="report(photos)">Report All</button>
       <button class="btn" @click="$emit('close')">Close</button>
     </div>
     <PhotoModal ref="modalRef" :selected-photo="selectedPhoto" />
@@ -20,25 +20,28 @@ defineProps<{
   photos: Photo[];
 }>();
 
-function approveOne(photo) {
-  //backend stuff
-  //photo now shown in /photos and admin main page
-  //remove "pending" label from user main page - notifies user
+function approve(photos: Photo[]) {
+  if (photos.length === 1) {
+    const photo = photos[0];
+    //backend stuff
+    //photo now shown in /photos and admin main page
+    //remove "pending" label from user main page - notifies user
+  } else {
+    //backend stuff
+    //same as for one photo, but w/ photos.forEach
+  }
 }
 
-function reportOne(photo) {
-  //backend stuff
-  // photo deleted?
-  // notifies user
-  // add smth to keep count of user's # of reported photos
-}
-
-function approveAll(photos) {
-  // same as approveOne(photo) but for an array
-}
-
-function reportAll(photos) {
-  // same as reportOne(photo) but for an array
+function report(photos: Photo[]) {
+  if (photos.length === 1) {
+    const photo = photos[0];
+    //backend stuff
+    // photo deleted?
+    // notifies user
+    // add smth to keep count of user's # of reported photos
+  } else {
+    // same thing but for array
+  }
 }
 
 const selectedPhoto = ref<Photo>();
