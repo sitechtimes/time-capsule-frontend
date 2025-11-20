0f74e09ddfe0711ff4cb8142320dc64c40123b34
christina chen <158211764+christinac09@users.noreply.github.com>
2025-11-20 11:02:19 -0500

    edit backup emIail

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -38,16 +38,25 @@
             <img src="/person-circle-outline.svg" aria-hidden="true" class="h-5 w-5 select-none dark:invert" draggable="false" />
           </div>
         </button>
-        <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-100 p-2 shadow">
-          <li>{{ user?.firstName }} {{ user?.lastName }}</li>
-          <li>
-            {{ user?.email }}
+        <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-100 p-2 shadow" @mousedown.prevent>
+          <li class="mx-2">{{ user?.firstName }} {{ user?.lastName }}</li>
+          <li class="mx-2">{{ user?.email }}</li>
+
+          <li class="flex flex-col">
+            <div v-if="!editing">
+              Backup email: <span>{{ email || "N/A (Click edit to add one)" }}</span>
+              <button class="ml-2 text-blue-600 underline hover:text-blue-800" @click="startEdit">Edit</button>
+            </div>
+
+            <div v-else class="mt-2 flex items-center gap-2">
+              <input v-model="tempEmail" type="text" class="rounded border border-gray-300 px-2 py-1" />
+              <button class="rounded bg-blue-600 px-3 py-1 text-white hover:bg-blue-700" @click="saveEmail">Save</button>
+              <button class="rounded bg-gray-200 px-3 py-1 text-gray-700 hover:bg-gray-300" @click="cancelEdit">Cancel</button>
+            </div>
           </li>
-          <li>Backup email: (how to access?) and add link to edit here</li>
-          <li class="font-bold">Settings</li>
-          <li>Light/dark mode</li>
-          <li>Change password</li>
-          <li><a>Notifications (add smth to indicate new notifs)</a></li>
+
+          <li><a>Change password</a></li>
+          <li><a>Notifications</a></li>
           <li><a @click="handleLogout">Logout</a></li>
         </ul>
       </div>
@@ -67,6 +76,25 @@ function toggle() {
 function handleLogout() {
   store.signOut();
   void router.push("/login");
+}
+
+const email = ref("bleh");
+const editing = ref(false);
+const tempEmail = ref(email.value);
+
+function startEdit() {
+  tempEmail.value = email.value;
+  editing.value = true;
+}
+
+function saveEmail() {
+  email.value = tempEmail.value;
+  editing.value = false;
+}
+
+function cancelEdit() {
+  tempEmail.value = email.value;
+  editing.value = false;
 }
 </script>
 
